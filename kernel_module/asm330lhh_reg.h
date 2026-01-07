a9cbbe55efb14438c422d6076750e84225575efb
Nguyen Nhan <112737822+jeremynguyenn@users.noreply.github.com>
2026-01-07 22:17:03 +0700

    Modify author and remove copyright from header
    Updated author information and removed copyright notice.

--- kernel_module/asm330lhh_reg.h
+++ kernel_module/asm330lhh_reg.h
@@ -1,22 +1,11 @@
 /*
   ******************************************************************************
   * @file    asm330lhh_reg.h
-  * @author  Sensor Solutions Software Team
+  * @author  Nguyen Nhan
   * @brief   This file contains all the functions prototypes for the
   *          asm330lhh_reg.c driver.
   ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+*/
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef ASM330LHH_REGS_H
